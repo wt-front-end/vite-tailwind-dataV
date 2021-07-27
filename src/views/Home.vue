588c55faed1f7f8de208176f1a6f49d329d70376
superDragon <admin@jixiaokang.com>
2021-07-27 11:47:56 +0800

    init: 初始化项目

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<template>
+  <main>
+    <div class="px-4 py-6 sm:px-0">
+      <div
+        class="border-4 border-dashed border-gray-200 rounded-lg h-96 p-4 text-center text-gray-400"
+      >
+        Here goes your content. You can also go the About page.
+      </div>
+    </div>
+    <el-button type="primary">主要按钮</el-button>
+  <el-button type="success">成功按钮</el-button>
+  <el-button type="info">信息按钮</el-button>
+  <el-button type="warning">警告按钮</el-button>
+  <el-button type="danger">危险按钮</el-button>
+  </main>
+</template>
---
+++ vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import styleImport from 'vite-plugin-style-import'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    styleImport({
+      libs: [
+        {
+          libraryName: 'element-plus',
+          resolveStyle: (name) => {
+            return `element-plus/lib/theme-chalk/${name}.css`;
+          },
+          resolveComponent: (name) => {
+            return `element-plus/lib/${name}`;
+          },
+        }
+      ]
+    })
+  ]
+})
